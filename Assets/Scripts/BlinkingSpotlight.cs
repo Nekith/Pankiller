fde9f5ceb3f6fa30061372f2a944bda57528fad0
Valerian Cubero <nekith@gmail.com>
2014-11-11 17:07:18 +0100

    Carrot navigation

--- Assets/Scripts/BlinkingSpotlight.cs
+++ Assets/Scripts/BlinkingSpotlight.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class BlinkingSpotlight : MonoBehaviour
 {
-	private Light light;
+	private Light bulb;
 	private float timer;
 	private float duration;
 	private int flicks;
 
 	void Start()
 	{
-		light = GetComponent<Light>();
+        bulb = GetComponent<Light>();
 		duration = Random.Range(2.5f, 5.0f);
 		flicks = 3;
 	}
@@ -23,16 +23,16 @@ public class BlinkingSpotlight : MonoBehaviour
 				duration = 0.1f;
 				timer = 0f;
 				if (flicks % 2 == 1) {
-					light.intensity = 0f;
+                    bulb.intensity = 0f;
 				} else {
-					light.intensity = 8f;
+                    bulb.intensity = 8f;
 				}
 				flicks -= 1;
 			} else {
 				duration = Random.Range(3.0f, 7.0f);
 				flicks = 3;
 				timer = 0f;
-				light.intensity = 8f;
+                bulb.intensity = 8f;
 			}
 		}
 	}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    public bool raging = false;
+    public float minVelocityFall = -13f;
+
+    //private CharacterController characterController;
+	private GameObject player;
+    private NavMeshAgent agent;
+    private float verticalVelocity = 0;
+
+	void Start()
+	{
+        //characterController = GetComponent<CharacterController>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update()
+    {
+        Gravity();
+        Navigation();
+    }
+
+    void Navigation()
+    {
+        if (raging == false) {
+            Vector3 distance = transform.position - player.transform.position;
+            if (Mathf.Abs(distance.y) <= 5 && distance.magnitude <= 20f) {
+                raging = true;
+                agent.SetDestination(player.transform.position);
+            }
+        } else {
+            agent.SetDestination(player.transform.position);
+        }
+	}
+
+    void Gravity()
+    {
+        if (verticalVelocity > minVelocityFall) {
+            verticalVelocity += 2.0f * Physics.gravity.y * Time.fixedDeltaTime;
+        }
+        Vector3 realSpeed = new Vector3(0f, verticalVelocity, 0f);
+        //characterController.Move(realSpeed * Time.deltaTime);
+    }
+}
